2da17190decd9cd428f58fbea29c09647ca30176
kl2500040080 <2500040080@kluniversity.in>
2026-08-06 09:23:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText(){
+
+    document.getElementById("demo").innerHTML =
+    "Hello from JavaScript!";
+}
+
+document.getElementById("hello").innerHTML =
+"Hello from External JS!";
+
+document.getElementById("line").innerHTML =
+"This line came from JavaScript of Home page.";
